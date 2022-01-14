6ea7f63b756edef3fd2b6d3daf83b27ec644a221
Paulo Elienay II <elie9alb@gmail.com>
2022-01-14 11:42:29 -0300

    feat: better echos

---
+++ posix-echos.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+red="\e[0;91m"
+blue="\e[0;94m"
+green="\e[0;92m"
+reset="\e[0m"
+
+# better echos
+notice() {
+    echo -e "${red}[!]${reset} $1";
+}
+
+okay() {
+    echo -e "[${green}OK${reset}] $1";
+}
+
+doing() {
+    echo -e "${green}-${2:-""}->${reset} $1";
+}
+
+skipping() {
+    echo -e "${blue}-${2:-""}->${reset} $1";
+}
